24dcf25740640625552ac91f2aaf543c2bd246b4
Tim Wu <tim.wu.0@gmail.com>
2015-04-30 15:03:42 -0700

    Fix TestEntity

--- test-entity/server/src/main/java/org/terracotta/TestServerEntity.java
+++ test-entity/server/src/main/java/org/terracotta/TestServerEntity.java
@@ -1,12 +1,19 @@
 package org.terracotta;
 
+import org.terracotta.entity.NoShardingStrategy;
 import org.terracotta.entity.ServerEntity;
 import org.terracotta.entity.ClientID;
+import org.terracotta.entity.ShardingStrategy;
 
 /**
  * @author twu
  */
 public class TestServerEntity implements ServerEntity {
+  @Override
+  public ShardingStrategy getShardingStrategy() {
+    return new NoShardingStrategy();
+  }
+
   @Override
   public void connected(ClientID clientID) {
   }
