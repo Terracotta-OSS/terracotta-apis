980651f22d6b84cee3cd1317a4f41b15ef4110ce
Jeff Disher <jdis@MCJDIS01.local>
2015-05-06 13:49:52 -0400

    TAB-5859 : Add endpoint close() support
    -add close() to EntityClientEndpoint interface since it will be the component which knows how to describe itself to the platform service when an Entity needs to close()

--- entity-client-api/src/main/java/org/terracotta/entity/EntityClientEndpoint.java
+++ entity-client-api/src/main/java/org/terracotta/entity/EntityClientEndpoint.java
@@ -3,11 +3,17 @@ package org.terracotta.entity;
 /**
  * @author twu
  */
-public interface EntityClientEndpoint {
+public interface EntityClientEndpoint extends AutoCloseable {
 
   byte[] getEntityConfiguration();
 
   void registerListener(EndpointListener listener);
 
   InvocationBuilder beginInvoke();
+  
+  /**
+   * The instance will be unusable after this call.
+   */
+  @Override
+  void close();
 }
