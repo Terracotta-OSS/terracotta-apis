9465195931902ac11e964bfa8feae259863580f8
Jeff Disher <jdis@MCJDIS01.local>
2015-05-19 17:55:44 -0400

    Remove acquireEntity call which had no valid implementations.

--- src/main/java/org/terracotta/connection/entity/EntityRef.java
+++ src/main/java/org/terracotta/connection/entity/EntityRef.java
@@ -12,16 +12,6 @@ public interface EntityRef<T extends Entity, C> {
   T fetchEntity();
 
   /**
-   * Gets the entity with the specified configuration. If the entity does not yet exist, return null. If the entity exists
-   * with a matching configuration return it. If the entity exists with a non-matching configuration throw an exception.
-   *
-   * @param configuration configuration to check against
-   * @return entity if it exists and matches the configuration
-   * @throws ConfigurationMismatchException thrown if an entity exists with a conflicting configuration.
-   */
-  T acquireEntity(C configuration) throws ConfigurationMismatchException;
-
-  /**
    * Gets the name of the entity
    *
    * @return name
