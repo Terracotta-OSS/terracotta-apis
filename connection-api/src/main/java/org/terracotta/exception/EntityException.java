2894be5a27dc66a3a97fe79e81c4645eb8aefc31
Jeff Disher <jdis@MCJDIS01.local>
2015-10-19 17:42:56 -0400

    TAB-6417 : changed abstract constructor from public to protected

--- connection-api/src/main/java/org/terracotta/exception/EntityException.java
+++ connection-api/src/main/java/org/terracotta/exception/EntityException.java
@@ -4,7 +4,7 @@ package org.terracotta.exception;
 public abstract class EntityException extends Exception {
   private static final long serialVersionUID = 1L;
   
-  public EntityException(String className, String entityName, String description) {
+  protected EntityException(String className, String entityName, String description) {
     super("Entity: " + className + ":" + entityName + " " + description);
   }
 }
