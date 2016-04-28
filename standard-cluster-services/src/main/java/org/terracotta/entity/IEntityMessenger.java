09bdd3fd3c8765e91aff4080b34b975db15b8d4f
Jeff Disher <jeff.disher@softwareag.com>
2016-04-28 14:52:02 -0400

    Fix #73: Introduced the IEntityMessenger standard cluster service

---
+++ standard-cluster-services/src/main/java/org/terracotta/entity/IEntityMessenger.java
@@ -0,0 +1,40 @@
+/*
+ *
+ *  The contents of this file are subject to the Terracotta Public License Version
+ *  2.0 (the "License"); You may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *
+ *  http://terracotta.org/legal/terracotta-public-license.
+ *
+ *  Software distributed under the License is distributed on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ *  the specific language governing rights and limitations under the License.
+ *
+ *  The Covered Software is Entity API.
+ *
+ *  The Initial Developer of the Covered Software is
+ *  Terracotta, Inc., a Software AG company
+ *
+ */
+package org.terracotta.entity;
+
+import com.tc.classloader.CommonComponent;
+
+
+/**
+ * The common interface for a built-in service which provides the ability for an entity to asynchronously message itself.
+ * This is also the underlying mechanism which allows entities to contact each other (as they can safely expose code which
+ * doesn't touch their state, but calls through to this interface, thus allowing a future invocation).
+ * This is because an entity can't safely call into another one since its execution state is undefined, relative to another
+ * entity, and any action taken by such a cross-entity call would not be replicated to passive entities.
+ */
+@CommonComponent
+public interface IEntityMessenger {
+  /**
+   * Asynchronously send a message to the entity instance which looked up the service instance.
+   * 
+   * @param message The message to send.
+   * @throws MessageCodecException The message could not be serialized.
+   */
+  void messageSelf(EntityMessage message) throws MessageCodecException;
+}
