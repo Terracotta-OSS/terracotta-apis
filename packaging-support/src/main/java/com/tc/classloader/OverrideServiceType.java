d0dffc44c23dd423c8e39206949e054271396b6b
Myron Scott <myron.scott@softwareag.com>
2017-10-10 08:54:18 -0700

    Add an override method using types

---
+++ packaging-support/src/main/java/com/tc/classloader/OverrideServiceType.java
@@ -0,0 +1,38 @@
+/*
+ *
+ *  The contents of this file are subject to the Terracotta Public License Version
+ *  2.0 (the "License"); You may not use this file except in compliance with the
+ *  License. You may obtain a copy of the License at
+ *
+ *  http://terracotta.org/legal/terracotta-public-license.
+ *
+ *  Software distributed under the License is distributed on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ *  the specific language governing rights and limitations under the License.
+ *
+ *  The Covered Software is Terracotta Core.
+ *
+ *  The Initial Developer of the Covered Software is
+ *  Terracotta, Inc., a Software AG company
+ *
+ */
+package com.tc.classloader;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Tagging a Service type with an override causes the classloader to ignore the 
+ * overridden types for the type provided.  The value is a fully qualified class name 
+ * which is to be overridden.
+ * 
+ */
+@Target( ElementType.TYPE )
+@Retention( RetentionPolicy.RUNTIME )
+@Repeatable( OverrideServiceTypes.class )
+public @interface OverrideServiceType {
+  Class<?> value();
+}
