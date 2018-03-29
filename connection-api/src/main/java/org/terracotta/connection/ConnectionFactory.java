6046c9430f27718a4d097562848560e90cba5d8e
Saurabh Agarwal <Saurabh.Agarwal@softwareag.com>
2018-03-29 13:36:56 +0530

    Issue#284: Added parallel APIs to ConnectionFactory.connect and ConnectionService.connect

--- connection-api/src/main/java/org/terracotta/connection/ConnectionFactory.java
+++ connection-api/src/main/java/org/terracotta/connection/ConnectionFactory.java
@@ -18,6 +18,7 @@
  */
 package org.terracotta.connection;
 
+import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Properties;
@@ -28,25 +29,43 @@ import java.util.ServiceLoader;
  */
 public final class ConnectionFactory {
   /**
-   * Establish a connection based on the given uri. The method will attempt to look for the first suitable implementation
-   * of a {@link org.terracotta.connection.ConnectionService} based on whether or not it handles the given URI.
+   * Establish a connection based on the given uri.
+   * <p>
+   * The method will attempt to look for the first suitable implementation of a {@link org.terracotta.connection.ConnectionService}
+   * based on whether or not it handles the given URI. {@link #connect(Iterable, Properties)} is preferable to this method,
+   * as the former does not involve extra parsing and enables you to provide multiple IPv6 addresses to connect to.
    *
-   * @param uri URI to connect to
+   * @param uri        URI to connect to
    * @param properties any configurations to be applied (implementation specific)
    * @return an established connection
    * @throws ConnectionException if there is an error while attempting to connect
+   * @see #connect(Iterable, Properties)
    */
   public static Connection connect(URI uri, Properties properties) throws ConnectionException {
     validateURI(uri);
+    return getConnectionService(uri).connect(uri, properties);
+  }
 
-    ServiceLoader<ConnectionService> serviceLoader = ServiceLoader.load(ConnectionService.class,
-        ConnectionFactory.class.getClassLoader());
-    for (ConnectionService connectionService : serviceLoader) {
-      if (connectionService.handlesURI(uri)) {
-        return connectionService.connect(uri, properties);
-      }
-    }
-    throw new IllegalArgumentException("Unknown URI " + uri);
+  /**
+   * Establish a connection to the provided servers.
+   * <p>
+   * The method will attempt to look for the first suitable implementation of a {@link org.terracotta.connection.ConnectionService}
+   * based on whether or not it handles the given connection type.
+   * This method is preferable to {@link #connect(URI, Properties)}, as it does not involve extra parsing and enables
+   * you to provide multiple IPv6 addresses to connect to.
+   * <p>
+   * {@link InetSocketAddress#createUnresolved(String, int)} can be used to populate an {@code Iterable} to be passed to
+   * this method. If the default server port is to be used, a value of 0 can be passed as the second argument to
+   * {@link InetSocketAddress#createUnresolved(String, int)}.
+   *
+   * @param servers    servers to connect to
+   * @param properties any configurations to be applied (implementation specific), including a connection type
+   * @return an established connection
+   * @throws ConnectionException if there is an error while attempting to connect
+   * @see #connect(URI, Properties)
+   */
+  public static Connection connect(Iterable<InetSocketAddress> servers, Properties properties) throws ConnectionException {
+    return getConnectionService(properties).connect(servers, properties);
   }
 
   private static void validateURI(URI uri) throws ConnectionException {
@@ -55,5 +74,28 @@ public final class ConnectionFactory {
     } catch (URISyntaxException e) {
       throw new ConnectionException(e);
     }
+  }
+
+  private static ConnectionService getConnectionService(Properties properties) {
+    String connectionType = properties.getProperty(ConnectionPropertyNames.CONNECTION_TYPE, "terracotta");
+    for (ConnectionService connectionService : getServiceLoader()) {
+      if (connectionService.handlesConnectionType(connectionType)) {
+        return connectionService;
+      }
+    }
+    throw new IllegalArgumentException("Unknown connection type " + connectionType);
+  }
+
+  private static ConnectionService getConnectionService(URI uri) {
+    for (ConnectionService connectionService : getServiceLoader()) {
+      if (connectionService.handlesURI(uri)) {
+        return connectionService;
+      }
+    }
+    throw new IllegalArgumentException("Unknown URI " + uri);
+  }
+
+  private static ServiceLoader<ConnectionService> getServiceLoader() {
+    return ServiceLoader.load(ConnectionService.class, ConnectionFactory.class.getClassLoader());
   }
 }
